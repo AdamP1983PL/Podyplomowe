cd2f903e94be809fc1a260e678e9ba1997131825
podlaski <podlaski83@gmail.com>
2022-10-09 14:20:27 +0200

    Simple program a + b

---
+++ src/Podyplomowe.java
@@ -0,0 +1,8 @@
+public class Podyplomowe {
+    public static void main(String[] args) {
+        int a =5;
+        int b = 6;
+        int result = a + b;
+        System.out.println(result);
+    }
+}
