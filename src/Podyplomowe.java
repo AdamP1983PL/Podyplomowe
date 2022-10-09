b0b67d5ba3fbc7899d334a6c0f8894e5d5fcefbe
podlaski <podlaski83@gmail.com>
2022-10-09 14:24:30 +0200

    Simple program a + b

--- src/Podyplomowe.java
+++ src/Podyplomowe.java
@@ -1,6 +1,7 @@
 public class Podyplomowe {
     public static void main(String[] args) {
-        int a =5;
+        // program do obliczania sumy a = 5 i b = 6
+        int a = 5;
         int b = 6;
         int result = a + b;
         System.out.println(result);
